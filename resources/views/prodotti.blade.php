81d69fe4f9d4de779373a0db85ad6ee6fe13bb2b
Sabrina <sabrinacunsolo85@gmail.com>
2020-12-22 22:03:49 +0100

    inserite categorie pasta

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -3,21 +3,52 @@
 @section('titolo-pagina', 'Homepage - Prodotti - La Molisana')
 
 @section('content')
-    <main>
+    <main id="main-container">
         <div class="container">
-            <p>pagina prodotti:</p>
+            <h3>Le lunghe</h3>
             <div class="card-container">
-                    @foreach ($pasta as $tipo_pasta)
+                @foreach ($pasta as $tipo_pasta)
+                    @if ($tipo_pasta['tipo'] == 'lunga')
                         <div class="card">
                             <img src="{{ $tipo_pasta['src'] }}" alt="{{ $tipo_pasta['titolo'] }}">
-
                             <div class="card-hover">
                                 <a href="#">
                                     {{ $tipo_pasta['titolo'] }}
                                 </a>
                             </div>
                         </div>
-                    @endforeach
+                    @endif
+                @endforeach
+            </div>
+            <h3>Le corte</h3>
+            <div class="card-container">
+                @foreach ($pasta as $tipo_pasta)
+                    @if ($tipo_pasta['tipo'] == 'corta')
+                        <div class="card">
+                            <img src="{{ $tipo_pasta['src'] }}" alt="{{ $tipo_pasta['titolo'] }}">
+                            <div class="card-hover">
+                                <a href="#">
+                                    {{ $tipo_pasta['titolo'] }}
+                                </a>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+            <h3>Le cortissime</h3>
+            <div class="card-container">
+                @foreach ($pasta as $tipo_pasta)
+                    @if ($tipo_pasta['tipo'] == 'cortissima')
+                        <div class="card">
+                            <img src="{{ $tipo_pasta['src'] }}" alt="{{ $tipo_pasta['titolo'] }}">
+                            <div class="card-hover">
+                                <a href="#">
+                                    {{ $tipo_pasta['titolo'] }}
+                                </a>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
             </div>
         </div>
     </main>
