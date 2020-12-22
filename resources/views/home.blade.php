c0706655ac5a3eaec33f8cf89cacf13604b9aafa
Sabrina <sabrinacunsolo85@gmail.com>
2020-12-22 14:50:50 +0100

    inizio esercizio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel Molisana</title>
+        <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
+    </head>
+    <body>
+        
+        <script src=" {{ asset('js/app.js') }} " charset="utf-8"></script>
+    </body>
+</html>
